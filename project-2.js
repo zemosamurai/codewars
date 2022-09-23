f88a943ed0a7f8aaf82efca2a6d03d5e2afd15e6
zemosamurai <zemosamurai@gmail.com>
2022-09-23 19:37:41 +0300

    Convert boolean values to strings 'Yes' or 'No'.

---
+++ project-2.js
@@ -0,0 +1,8 @@
+// Convert boolean values to strings 'Yes' or 'No'.
+function boolToWord(bool) {
+   if (bool === true) {
+      return 'Yes';
+   } else if (bool === false) {
+      return 'No';
+   }
+}
